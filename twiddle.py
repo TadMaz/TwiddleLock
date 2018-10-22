73f5a7d697ae30982b535bf07658b557ad345a0e
nyashabryan <nyashabryan23@gmail.com>
2018-10-22 20:52:24 +0200

    Fixed bug

--- twiddle.py
+++ twiddle.py
@@ -130,16 +130,16 @@ def unsecure_mode():
     global pi, TICK, DURATIONS
     print("Starting unsecure mode")
     reading = MCP.read(0) # POT is on channel 0
-    while (round(ADCPOT(MCP.read(0)), 2) == reading):
+    while (round(ADCPOT(MCP.read_adc(0)), 2) == reading):
         pass
     TICK = time.monotonic()
     print("Now taking readings")
     while(len(DURATIONS)< len(KEY)):
-        while (reading != round(ADCPOT(MCP.read(0)), 2)):
+        while (reading != round(ADCPOT(MCP.read_adc(0)), 2)):
             reading = MCP.read(0)
             time.sleep(1/FREQ)
         DURATIONS.append(time.monotonic() - TICK)
-        while(reading == round(ADCPOT(MCP.read(0)), 2)):
+        while(reading == round(ADCPOT(MCP.read_adc(0)), 2)):
             pass
     print("Code entered")
     DURATIONS.sort()
