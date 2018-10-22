1c351a5902e1b0cb41f3d9496109994acbdecc7d
Tadiwanashe Mazara <tadiwamazara@gmail.com>
2018-10-22 16:15:20 +0200

    Changed continue statements to pass

---
+++ sort.s
@@ -0,0 +1,11 @@
+/* Integer sort function in ARM assembly */
+/* 22 October 2018 */
+
+
+/* Start the text section of the program */
+.text
+
+
+.global main
+main:
+
--- twiddle.py
+++ twiddle.py
@@ -182,18 +182,15 @@ class Directions(threading.Thread):
             sleep(SAMPLING_PERIOD)
             if( values[0]-values[1]>0.1 ):                     #when values increase->left
                 while(values[0]-values[1] >0.1):              #check whether it is still increasing
-                    sleep(0.05)                      
-                    continue
+                    pass
                 print("L")
             elif( abs(values[0]-values[1] )<0.1):
                 while( abs(values[0]-values[1] )<0.1 ):              #check whether it is still increasing
-                    sleep(0.05)                      
-                    continue
+                    pass
                 print("constant")
             elif ( values[0]-values[1]<-0.1 ):                 #when values decrease->right
                 while(values[0]-values[1] <-0.1):              #check whether it is still increasing
-                    sleep(0.05)                      
-                    continue
+                    pass
                 print("R")                          
                                           
 if __name__ == "__main__":
